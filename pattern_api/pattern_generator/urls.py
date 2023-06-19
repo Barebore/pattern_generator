ae1d438ee0f452d16228577cbfb5dc4279d23f3e
Борислав Печерских <borislavtarget@gmail.com>
2023-06-19 12:25:53 +0500

    change /api/ in django to api_pattern

--- pattern_api/pattern_generator/urls.py
+++ pattern_api/pattern_generator/urls.py
@@ -4,6 +4,6 @@ from .views import PatternAPIViewBase64, TestPatternAPIView
 app_name = 'pattern_generator'
 
 urlpatterns = [
-    path('api/v1/test/', TestPatternAPIView.as_view()),
-    path('api/v1/make_pattern_base64', PatternAPIViewBase64.as_view()),
+    path('api_pattern/v1/test/', TestPatternAPIView.as_view()),
+    path('api_pattern/v1/make_pattern_base64', PatternAPIViewBase64.as_view()),
 ]
